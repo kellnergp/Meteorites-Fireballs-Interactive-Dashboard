84d8cb884e62c612e564f0f1ee6b5481660c82a0
Joseph March <josephmarch@gmail.com>
2021-11-06 10:45:35 -0400

    Updated Flask Work to include barlogic.

--- Flask Work/app.py
+++ Flask Work/app.py
@@ -12,6 +12,7 @@ textheader = "Meteorites and Fireballs"
 # Read CSVs into dictionaries
 meteorite_data = pd.read_csv("Resources/Meteorite_Landings.csv")
 fireball_data = pd.read_csv("Resources/Fireball_And_Bolide_Reports.csv")
+meteorite_count = pd.read_csv("Resources/Meteorites_Count.csv")
 
 # Setup mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/meteorite_app"
@@ -20,16 +21,25 @@ mongo = PyMongo(app)
 # Populate mongo database
 meteorite_dict = mongo.db.meteorite_dict
 fireball_dict = mongo.db.fireball_dict
+meteorite_count_dict = mongo.db.meteorite_count_dict
 meteorite_dict.delete_many({})
 fireball_dict.delete_many({})
+meteorite_count_dict.delete_many({})
 meteorite_dict.insert_many(meteorite_data.to_dict('records'))
 fireball_dict.insert_many(fireball_data.to_dict('records'))
-#meteorite_dict.update({}, meteorite_data, upsert=True)
-#fireball_dict.update({}, fireball_data, upsert=True)
+meteorite_count_dict.insert_many(meteorite_count.to_dict('records'))
 
-# Pull dataframes from mongo database for use
-meteorites = pd.DataFrame(list(meteorite_dict.find()))
-fireballs = pd.DataFrame(list(fireball_dict.find()))
+# Pull data from mongo database for use
+meteorites = []
+fireballs = []
+
+m_years = []
+m_count = []
+cur = meteorite_count_dict.find()
+for i in cur:
+    m_years.append(i["year"])
+    m_count.append(i["count"])
+
 # Route that renders index.html template
 @app.route("/")
 def echo():
@@ -38,7 +48,7 @@ def echo():
 # Route for data
 @app.route("/data")
 def data():
-    return render_template("data.html", textheader=textheader, meteorites=meteorites, fireballs=fireballs)
+    return render_template("data.html", textheader=textheader, meteorites=meteorites, fireballs=fireballs, m_years=m_years, m_count=m_count)
 
 # Route for map
 @app.route("/map")
